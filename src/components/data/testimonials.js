eaa13c641f46507b501b6ca21a27012cf7a7a63a
Kelli Libin <km.putnam2@gmail.com>
2023-09-18 15:24:33 -0600

    created the data for reviews

---
+++ src/components/data/testimonials.js
@@ -0,0 +1,24 @@
+const reviewData = [
+  {
+    name: "John Doe",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna molestie at elementum eu facilisis sed.",
+  },
+  {
+    name: "Jane Doe",
+    content:
+      "Erat pellentesque adipiscing commodo elit at. Pretium fusce id velit ut tortor pretium viverra suspendisse. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Et pharetra pharetra massa massa ultricies. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Quam vulputate dignissim suspendisse in est ante. Odio tempor orci dapibus ultrices in. Consectetur purus ut faucibus pulvinar elementum integer enim. Ac turpis egestas maecenas pharetra convallis. ",
+  },
+  {
+    name: "Steve",
+    content:
+      "Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Morbi leo urna molestie at elementum eu facilisis. Sit amet consectetur adipiscing elit ut. In hac habitasse platea dictumst quisque sagittis. Volutpat blandit aliquam etiam erat. Eu scelerisque felis imperdiet proin fermentum.",
+  },
+  {
+    name: "James",
+    content:
+      "Arcu non odio euismod lacinia at quis. Erat imperdiet sed euismod nisi porta lorem mollis aliquam. Urna cursus eget nunc scelerisque viverra mauris. Nibh praesent tristique magna sit amet purus gravida quis blandit. Fames ac turpis egestas sed tempus urna et. Tellus cras adipiscing enim eu turpis. Fringilla ut morbi tincidunt augue interdum.",
+  },
+];
+
+export default reviewData;
