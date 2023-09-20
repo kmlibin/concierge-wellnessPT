c4be40d5fda9d30710e23027041ce42088d9069d
Kelli Libin <km.putnam2@gmail.com>
2023-09-20 17:22:31 -0600

    added the outlet for rrd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Outlet} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
 
 const App = () => {
+AOS.init(); 
   return (
-    <div>App</div>
+   <>
+   <NavBar />
+ 
+    <Outlet />
+   
+   <Footer/>
+   </>
   )
 }
 
