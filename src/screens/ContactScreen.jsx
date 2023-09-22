35b852c531f93b92f46c3ae85689d9294c1afb60
Kelli Libin <km.putnam2@gmail.com>
2023-09-22 13:04:24 -0600

    moved to two column layout, bg photo

--- src/screens/ContactScreen.jsx
+++ src/screens/ContactScreen.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
 import "./ContactScreen.scss";
+import { TfiEmail } from "react-icons/tfi";
+import chickenman from "../assets/chickenman.jpg";
 
 const ContactScreen = () => {
   return (
     <main className="contact">
+      <div className="image-container"></div>
       <div className="contact-text">
-        <h2>Let's Connect</h2>
-        <span className="hr-line"></span>
-        <p>
-          Please tell us about your problem in the form below and Dr. Wade will
-          reach out and set up a time to connect!
-        </p>
+        <div className="centered-text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus
+            sed turpis tincidunt id. Vel quam elementum pulvinar etiam non quam
+            lacus. Mattis vulputate enim nulla aliquet porttitor lacus luctus
+            accumsan. Faucibus nisl tincidunt eget nullam non.
+          </p>
+        </div>
       </div>
       <ContactForm />
     </main>
