0230b99ba9c62fe470a9751aefdd52eed67e4530
Kelli Libin <km.putnam2@gmail.com>
2023-09-21 18:33:34 -0600

    added page title and text

--- src/screens/ContactScreen.jsx
+++ src/screens/ContactScreen.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ContactForm from '../components/ContactForm'
-import './ContactScreen.scss'
+import React from "react";
+import ContactForm from "../components/ContactForm";
+import "./ContactScreen.scss";
 
 const ContactScreen = () => {
   return (
-    <div className="contact">
-    <ContactForm />
-    </div>
-  )
-}
+    <main className="contact">
+      <div className="contact-text">
+        <h2>Let's Connect</h2>
+        <p>
+          Please tell us about your problem in the form below and Dr. Wade will
+          reach out and set up a time to connect!
+        </p>
+      </div>
+      <ContactForm />
+    </main>
+  );
+};
 
-export default ContactScreen
+export default ContactScreen;
