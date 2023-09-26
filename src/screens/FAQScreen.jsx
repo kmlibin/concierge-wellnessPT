31eccc583de871ab456280514ce8327d4d4b6e90
Kelli Libin <km.putnam2@gmail.com>
2023-09-26 13:22:12 -0600

    create

---
+++ src/screens/FAQScreen.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./FAQScreen.scss";
+import pullup from "../assets/pullup.jpg";
+
+const FAQScreen = () => {
+  return (
+    <main className="faq">
+      <div className="faq-title">
+        <h1>FAQs</h1>
+        <span className="hr-line"></span>
+      </div>
+      <div className="faq-container">
+        <div className="faq-text">
+          <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h4>
+          <p>
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lacus sed turpis tincidunt id. Vel quam elementum pulvinar etiam non
+            quam lacus. Mattis vulputate enim nulla aliquet porttitor lacus
+            luctus accumsan. Faucibus nisl tincidunt eget nullam non. Aliquam id
+            diam maecenas ultricies mi eget mauris.{" "}
+          </p>
+        </div>
+        <div className="faq-text">
+          <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h4>
+          <p>
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lacus sed turpis tincidunt id. Vel quam elementum pulvinar etiam non
+            quam lacus. Mattis vulputate enim nulla aliquet porttitor lacus
+            luctus accumsan. Faucibus nisl tincidunt eget nullam non. Aliquam id
+            diam maecenas ultricies mi eget mauris.{" "}
+          </p>
+        </div>
+        <div className="faq-text">
+          <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h4>
+          <p>
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lacus sed turpis tincidunt id. Vel quam elementum pulvinar etiam non
+            quam lacus. Mattis vulputate enim nulla aliquet porttitor lacus
+            luctus accumsan. Faucibus nisl tincidunt eget nullam non. Aliquam id
+            diam maecenas ultricies mi eget mauris.{" "}
+          </p>
+        </div>
+        <div className="faq-text">
+          <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h4>
+          <p>
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lacus sed turpis tincidunt id. Vel quam elementum pulvinar etiam non
+            quam lacus. Mattis vulputate enim nulla aliquet porttitor lacus
+            luctus accumsan. Faucibus nisl tincidunt eget nullam non. Aliquam id
+            diam maecenas ultricies mi eget mauris.{" "}
+          </p>
+        </div>
+        <div className="faq-text">
+          <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h4>
+          <p>
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lacus sed turpis tincidunt id. Vel quam elementum pulvinar etiam non
+            quam lacus. Mattis vulputate enim nulla aliquet porttitor lacus
+            luctus accumsan. Faucibus nisl tincidunt eget nullam non. Aliquam id
+            diam maecenas ultricies mi eget mauris.{" "}
+          </p>
+        </div>
+      </div>
+      {/* <div className="faq-image">
+        <img src = {pullup}/>
+      </div> */}
+    </main>
+  );
+};
+
+export default FAQScreen;
