ac4402d8938b51fa4a32aef1d5512049178a9ce3
Kelli Libin <km.putnam2@gmail.com>
2023-09-27 11:46:20 -0600

    create modal for form submit success/err

---
+++ src/components/Modal.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Modal.scss";
+import { IoClose } from "react-icons/io5";
+
+const Modal = ({ setIsOpen, submissionStatus, setSubmissionStatus }) => {
+  return (
+    <>
+      <div className="modal-backdrop" onClick={() => setIsOpen(false)} />
+      <div className="centered">
+        <div className="modal">
+          <div className="modal-header">
+            <h5 className="heading">{submissionStatus.heading}</h5>
+          </div>
+          <button
+            className="top-close-btn"
+            onClick={() => (setIsOpen(false), setSubmissionStatus(null))}
+          >
+            <IoClose style={{ marginBottom: "-3px" }} />
+          </button>
+          <div className="modal-content">{submissionStatus.message}</div>
+
+          <div className="actions-container">
+            <button
+              className="close-btn"
+              onClick={() => (setIsOpen(false), setSubmissionStatus(null))}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
