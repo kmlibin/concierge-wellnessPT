c5432dcd38a56504df50b4413fcfd6e984b3685a
Kelli Libin <km.putnam2@gmail.com>
2023-11-08 09:58:49 -0700

    added two reviews

--- src/components/Testimonials/reviewData.js
+++ src/components/Testimonials/reviewData.js
@@ -1,21 +1,23 @@
 const reviewData = [
   {
-    name: "John Doe",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna molestie at elementum eu facilisis sed.",
+    name: "Dylan T. ",
+    content: `Working with Christian Wade from Concierge Wellness has been a game-changer for me. From our very first session, it was clear that Christian isn’t just any physical therapist. He genuinely cares about his clients and goes above and beyond to offer solutions tailored to individual needs.
+      Choosing the cash payment option simplified everything for me, removing the usual insurance-related stress. What truly touched me was Christian's willingness to travel and ensure I was comfortable in my own environment. This personalized touch made a world of difference.
+      Beyond his exceptional skills, Christian has this incredible way of explaining complex concepts. Before meeting him, many aspects of physical therapy felt like they were in a foreign language. But he broke everything down, ensuring I understood every step of my treatment. This not only helped me physically but gave me peace of mind.
+      I can't express enough how grateful I am for Christian’s dedication and expertise. If you're considering physical therapy, look no further than Concierge Wellness. It's not just about the therapy; it's about the personal touch and genuine care.`,
   },
   {
-    name: "Jane Doe",
-    content:
-      "Erat pellentesque adipiscing commodo elit at. Pretium fusce id velit ut tortor pretium viverra suspendisse. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Et pharetra pharetra massa massa ultricies. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Quam vulputate dignissim suspendisse in est ante. Odio tempor orci dapibus ultrices in. Consectetur purus ut faucibus pulvinar elementum integer enim. Ac turpis egestas maecenas pharetra convallis. ",
+    name: "Keiriel N.",
+    content: `Christian, DPT has been awesome from the beginning of my treatment. Listens to my concerns and what I feel like needs to be better or what is going well and he always has my best interest in mind. Works towards my goals and always has patient-centered care. He is personable and meets me where I am every day I am there. Pushes me to do better within my limits and is always encouraging. 
+    I have never felt judgment coming in if I hadn't met my goal or got behind on my workouts. He is just as encouraging and understanding. I'm so thankful he has been my PT!!!`,
   },
   {
-    name: "Steve",
+    name: "Steve Doe",
     content:
       "Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Morbi leo urna molestie at elementum eu facilisis. Sit amet consectetur adipiscing elit ut. In hac habitasse platea dictumst quisque sagittis. Volutpat blandit aliquam etiam erat. Eu scelerisque felis imperdiet proin fermentum.",
   },
   {
-    name: "James",
+    name: "James Doe",
     content:
       "Arcu non odio euismod lacinia at quis. Erat imperdiet sed euismod nisi porta lorem mollis aliquam. Urna cursus eget nunc scelerisque viverra mauris. Nibh praesent tristique magna sit amet purus gravida quis blandit. Fames ac turpis egestas sed tempus urna et. Tellus cras adipiscing enim eu turpis. Fringilla ut morbi tincidunt augue interdum.",
   },
