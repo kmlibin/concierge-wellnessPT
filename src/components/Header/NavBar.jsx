5ebd7c92a2e9cc8f876de1fff7be5fe6b9199477
Kelli Libin <km.putnam2@gmail.com>
2023-11-08 15:20:28 -0700

    responsive navbar - hamburger menu at 900

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 //rrd
 import { Link } from "react-router-dom";
 //styles
 import "./NavBar.scss";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
 
+import logo1 from "../../assets/logo1.png";
 //routes for the links
 const navRoutes = [
   {
@@ -34,8 +36,15 @@ const navRoutes = [
 ];
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
   return (
     <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo1} alt="logo" />
+      </div>
       <ul className="navbar-links">
         {navRoutes.map((item) =>
           item.name === "Contact" ? (
@@ -49,6 +58,33 @@ const NavBar = () => {
           )
         )}
       </ul>
+      <div className="navbar-menu">
+        <HiMenuAlt4 onClick={handleToggle} />
+
+        {toggle && (
+          <div
+            className="navbar-menu-content"
+            data-aos="fade-in-right"
+            data-aos-duration="1000"
+          >
+            <HiX onClick={handleToggle} />
+
+            <ul>
+              {navRoutes.map((item) =>
+                item.name === "Contact" ? (
+                  <li className="contact-link" key={`link-${item.name}`}>
+                    <Link to={item.route}>{item.name}</Link>
+                  </li>
+                ) : (
+                  <li className="nav-link" key={`link-${item.name}`}>
+                    <Link to={item.route}>{item.name}</Link>
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
