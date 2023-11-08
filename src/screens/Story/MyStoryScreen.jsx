4e584bc9f2f5bd237dcf11474f78ee271ae0df1d
Kelli Libin <km.putnam2@gmail.com>
2023-11-08 10:49:42 -0700

    narrower, photos next to text horizontally

--- src/screens/Story/MyStoryScreen.jsx
+++ src/screens/Story/MyStoryScreen.jsx
@@ -5,7 +5,7 @@ import bands from "../../assets/bands.jpg";
 import weights from "../../assets/weights.jpg";
 import running from "../../assets/running.jpg";
 import fitness from "../../assets/fitness.jpg";
-import moreweights from "../../assets/moreweights.jpg";
+import pullup from "../../assets/pullup.jpg";
 import content from "../../constants/content";
 
 const MyStoryScreen = () => {
@@ -16,50 +16,19 @@ const MyStoryScreen = () => {
           <h1>{content.myStory.header}</h1>
           <span className="hr-line"></span>
         </div>
-        <p>
-          Cras ornare arcu dui vivamus arcu felis. Morbi non arcu risus quis
-          varius. Proin fermentum leo vel orci. Ac turpis egestas maecenas
-          pharetra convallis posuere morbi leo urna. Risus in hendrerit gravida
-          rutrum quisque non tellus orci. Cras ornare arcu dui vivamus arcu
-          felis. Morbi non arcu risus quis varius. Proin fermentum leo vel orci.
-          Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna.
-          Risus in hendrerit gravida rutrum quisque non tellus orci. Cras ornare
-          arcu dui vivamus arcu felis. Morbi non arcu risus quis varius. Proin
-          fermentum leo vel orci. Ac turpis egestas maecenas pharetra convallis
-          posuere morbi leo urna. Risus in hendrerit gravida rutrum quisque non
-          tellus orci.Cras ornare arcu dui vivamus arcu felis. Morbi non arcu
-          risus quis varius. Proin fermentum leo vel orci. Ac turpis egestas
-          maecenas pharetra convallis posuere morbi leo urna. Risus in hendrerit
-          gravida rutrum quisque non tellus orci.{" "}
-        </p>
-        <img src={moreweights} className="story-image" />
-
-        <p>
-          Metus aliquam eleifend mi in. Platea dictumst vestibulum rhoncus est
-          pellentesque elit ullamcorper dignissim. Tellus elementum sagittis
-          vitae et leo duis. Vulputate odio ut enim blandit volutpat maecenas
-          volutpat blandit aliquam. Libero volutpat sed cras ornare arcu dui.
-          Ultrices tincidunt arcu non sodales neque. Cras ornare arcu dui
-          vivamus arcu felis. Morbi non arcu risus quis varius. Proin fermentum
-          leo vel orci. Ac turpis egestas maecenas pharetra convallis posuere
-          morbi leo urna. Risus in hendrerit gravida rutrum quisque non tellus
-          orci.
-        </p>
-        <div className="multiple-images">
+        <div className="story-image-container">
+          <p>{content.myStory.contentOne}</p>
+          <img src={pullup} className="story-image" />
+        </div>
+        {/* <div className="story-image-container">      */}
+        <p style={{width: "80%", marginTop: "1.5rem"}}>{content.myStory.contentTwo}</p>
+          <div className="multiple-images">
           <img src={running} className="story-image" />
           <img src={running} className="story-image" />
         </div>
-
-        <p>
-          Ac feugiat sed lectus vestibulum mattis ullamcorper velit. Commodo
-          odio aenean sed adipiscing diam. Morbi blandit cursus risus at
-          ultrices mi. Malesuada pellentesque elit eget gravida. Tortor id
-          aliquet lectus proin nibh nisl condimentum id venenatis. Mauris
-          commodo quis imperdiet massa tincidunt nunc pulvinar sapien. Tincidunt
-          nunc pulvinar sapien et. Nulla facilisi morbi tempus iaculis urna id.
-          Vel pharetra vel turpis nunc eget. Tellus mauris a diam maecenas sed.
-          Auctor eu augue ut lectus.
-        </p>
+     
+          {/* <img src={running}  className="story-image" /> */}
+        {/* </div> */}
       </div>
     </article>
   );
