375f108b663f5ec09a5932cee5d690cd010e3b4e
Kelli Libin <km.putnam2@gmail.com>
2023-12-20 15:20:28 -0700

    implemented call button

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./HomeScreen.scss";
 import Testimonials from "../../components/Testimonials/Testimonials.jsx";
+import CallButton from "../../components/DiscoveryButton/CallButton.jsx";
 import logo2 from "../../assets/logo2.png";
 import fitness from "../../assets/fitness.jpg";
 import running from "../../assets/running.jpg";
@@ -35,13 +36,7 @@ const HomeScreen = () => {
             </h1>
             <h4>In home services available</h4>
           </div>
-          <a
-            className="contact-link call"
-            target="blank"
-            href="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1EETz6ot_qwrUVyS1SoXryFGwiVbG2x95K22gDuY4CVbJ1KHUqBRfEatmAd_TC_yjMB1cAE2eZ?gv=true"
-          >
-            Book FREE Discovery Call!
-          </a>
+          <CallButton />
         </div>
 
         <Testimonials />
@@ -134,29 +129,7 @@ const HomeScreen = () => {
           ></iframe>
         </div>
 
-        <div
-          className="schedule"
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            margin: "1rem",
-          }}
-        >
-          <iframe
-            src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1EETz6ot_qwrUVyS1SoXryFGwiVbG2x95K22gDuY4CVbJ1KHUqBRfEatmAd_TC_yjMB1cAE2eZ?gv=true"
-            title="Google Scheduling Calendar"
-            style={{
-              border: "0",
-              width: "80%",
-              height: "600px",
-              frameborder: "0",
-              background: "rgb(242,242,242)",
-              borderRadius: "5px",
-            }}
-          ></iframe>
-          "
-        </div>
+        
       </main>
     </>
   );
