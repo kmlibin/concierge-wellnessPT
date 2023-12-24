222ebc6e49979be68f760c9317a02a9e7c22bec9
Kelli Libin <km.putnam2@gmail.com>
2023-12-24 10:52:25 -0700

    split calendar into scss, responsive to 1024

--- src/components/GoogleCalendar/Calendar.jsx
+++ src/components/GoogleCalendar/Calendar.jsx
@@ -1,31 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./Calendar.scss";
 
 const Calendar = () => {
   return (
-    <div
-          className="schedule"
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            margin: "1rem",
-          }}
-        >
-          <iframe
-            src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1EETz6ot_qwrUVyS1SoXryFGwiVbG2x95K22gDuY4CVbJ1KHUqBRfEatmAd_TC_yjMB1cAE2eZ?gv=true"
-            title="Google Scheduling Calendar"
-            style={{
-              border: "0",
-              width: "60%",
-              height: "600px",
-              frameborder: "0",
-              background: "rgb(242,242,242)",
-              borderRadius: "5px",
-            }}
-          ></iframe>
-          "
-        </div>
-  )
-}
+    <div className="schedule">
+      <iframe
+        src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1EETz6ot_qwrUVyS1SoXryFGwiVbG2x95K22gDuY4CVbJ1KHUqBRfEatmAd_TC_yjMB1cAE2eZ?gv=true"
+        title="Google Scheduling Calendar"
+        className="calendar-container"
+        style={{
+          frameborder: "0",
+        }}
+      ></iframe>
+      "
+    </div>
+  );
+};
 
-export default Calendar
+export default Calendar;
