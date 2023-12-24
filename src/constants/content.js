ec912954d53244b802760e4f128867690d02b958
Kelli Libin <km.putnam2@gmail.com>
2023-12-24 10:20:45 -0700

    migrated text to content file

--- src/constants/content.js
+++ src/constants/content.js
@@ -3,15 +3,52 @@ const content = {
   about: {},
   myStory: {
     header: "Why Physical Therapy?",
-    contentOne: (<div>Ten years ago, while in college, I had a gnarly back injury that left me unable to get out of bed for two days. I had a long history of low back discomfort, but this was bad. It was scary, and I felt hopeless. The hopelessness only increased when my campus RN gave me a printout of exercises from the 1980s and told me not to lift more than 5 pounds… think about everyday items that weigh more than 5 pounds. I now know this advice is ridiculous. <br /> <br />
-        My journey in the traditional medical model had started. I went to PT and did stretches, clamshells, and bridges. Over time, my pain and function improved, but I couldn’t nearly function as I could before. I was depressed with my lack of function, and I wanted answers. I got an MRI showing I had three herniations. Testing gave me relief, knowing I wasn’t imagining this pain, but it made me believe I was fragile. I tried injections, traction, dry needling, massage, you name it. I even turned to alcohol to numb the pain. I still had significant back pain, and my day-to-day life as a college student had drastically changed. 
-        No longer were the five days/week in the gym and pickup basketball three days/week.</div>),
+    contentOne: (
+      <div>
+        Ten years ago, while in college, I had a gnarly back injury that left me
+        unable to get out of bed for two days. I had a long history of low back
+        discomfort, but this was bad. It was scary, and I felt hopeless. The
+        hopelessness only increased when my campus RN gave me a printout of
+        exercises from the 1980s and told me not to lift more than 5 pounds…
+        think about everyday items that weigh more than 5 pounds. I now know
+        this advice is ridiculous. <br /> <br />
+        My journey in the traditional medical model had started. I went to PT
+        and did stretches, clamshells, and bridges. Over time, my pain and
+        function improved, but I couldn’t nearly function as I could before. I
+        was depressed with my lack of function, and I wanted answers. I got an
+        MRI showing I had three herniations. Testing gave me relief, knowing I
+        wasn’t imagining this pain, but it made me believe I was fragile. I
+        tried injections, traction, dry needling, massage, you name it. I even
+        turned to alcohol to numb the pain. I still had significant back pain,
+        and my day-to-day life as a college student had drastically changed. No
+        longer were the five days/week in the gym and pickup basketball three
+        days/week.
+      </div>
+    ),
     contentTwo: `It wasn’t until one of my physical therapists and later mentors told me I needed to start getting back to my old hobbies. I was scared and angry, as if she didn’t believe me, but she was right. Over time, I gradually started doing the things I feared, and it only improved. Then, I went through PT school myself and found great colleagues and mentors along the way. Four years into being a Doctor of PT, one of my continuing education courses I took nailed it with its loading principles. I had avoided heavy lifting for so long… but that’s exactly what I needed.
         Now, whenever my back bothers me, or I’m achy after a long day at work or sitting in the car, I deadlift! I’ve found that weightlifting and staying active is the only thing that keeps my back pain at bay and makes me feel good, strong, and capable.`,
   },
   contact: {},
   services: {
     header: "Services",
+    subheading: "Concierge Physical Therapy Solutions",
+    summary:
+      "Empowering your journey to wellness, our dedicated physical therapy services offer personalized assessments and expert treatments. Discover a tailored approach for your optimal health",
+    listOne: [
+      "Myofascial decompression",
+      "Joint mobilization",
+      "Therapeautic massage",
+      "Corrective exercise prescription",
+      "Trigger point dry needling",
+      "Spinal manipulation",
+    ],
+    listTwo: [
+      "Individualized wellness coaching",
+      "Exercise programming",
+      "Corporate wellness",
+      "Ergonomic assessments",
+      "Group fitness classes",
+    ],
   },
 };
 
--- src/screens/Services/ServicesScreen.jsx
+++ src/screens/Services/ServicesScreen.jsx
@@ -3,11 +3,11 @@ import "./ServicesScreen.scss";
 import services from "../../assets/services.jpg";
 import report from "../../assets/report.png";
 import health from "../../assets/health.png";
-import kettlebell from '../../assets/kettlebell.png'
+import kettlebell from "../../assets/kettlebell.png";
 import ImageGallery from "../../components/ImageGallery/ImageGallery.jsx";
 import Imagegalltest from "../../components/ImageGallery/imagegalltest";
 import Calendar from "../../components/GoogleCalendar/Calendar.jsx";
-import content from '../../constants/content';
+import content from "../../constants/content";
 
 const ServicesScreen = () => {
   return (
@@ -25,42 +25,39 @@ const ServicesScreen = () => {
           <div className="services-main">
             <div className="services-text">
               <h1>{content.services.header}</h1>
-              <h4>Some Text Here About How Cool They Are</h4>
+              <h4>{content.services.subheading}</h4>
               <span className="hr-line"></span>
               <p>
-                Aliquam id diam maecenas ultricies mi eget mauris. Eget egestas
-                purus viverra accumsan in nisl. Elit scelerisque mauris
-                pellentesque pulvinar pellentesque habitant morbi tristique.
+                {content.services.summary}
               </p>
               <div className="service-list">
                 <ul>
-                  <li>item 1 goes here</li>
-                  <li>item 2</li>
-                  <li>item 3 is cool</li>
-                  <li>item 4 says a lot more</li>
-                  <li>item 5</li>
+                  {content.services.listOne.map((service) => (
+                    <li>{service}</li>
+                  ))}   
+                  {content.services.listTwo.map((service) => (
+                    <li>{service}</li>
+                  ))}
                 </ul>
-                <ul>
-                  <li>item 1 goes here</li>
-                  <li>item 2</li>
-                  <li>item 3 is cool</li>
-                  <li>item 4 says a lot more</li>
-                  <li>item 5</li>
-                </ul>
+                {/* <ul>
+                {content.services.listTwo.map((service) => (
+                    <li>{service}</li>
+                  ))}
+                </ul> */}
               </div>
             </div>
             <span className="hr-line"></span>
             <div className="services-icons">
               <div className="img-container">
-                <img src={report} alt="report"/>
+                <img src={report} alt="report" />
               </div>
               {/* <img src = {snip1} /> */}
               <div className="img-container">
-                <img src={kettlebell} alt="kettlebell"/>
+                <img src={kettlebell} alt="kettlebell" />
               </div>
 
               <div className="img-container">
-                <img src={health} alt="health"/>
+                <img src={health} alt="health" />
               </div>
               {/* <div className="img-container">
           <img src = {yoga} />
@@ -68,10 +65,10 @@ const ServicesScreen = () => {
             </div>
           </div>
         </div>
-      </main>   
+      </main>
       <ImageGallery />
-     <Imagegalltest />
-<Calendar />
+      <Imagegalltest />
+      <Calendar />
     </>
   );
 };
