97839439627545edf5400919765728259e151f4b
Kelli Libin <km.putnam2@gmail.com>
2023-12-24 16:24:38 -0700

    added rough partners section, fixed map

---
+++ src/constants/partners.js
@@ -0,0 +1,20 @@
+const partners =  [
+    {name: "lulu", logo: "assets/bands.jpg"},
+    {name: "otf", logo: "assets/bands.jpg"},
+    {name: "cssc", logo: "assets/bands.jpg"},
+    {name: "JPEL Farms", logo: "assets/bands.jpg"},
+    {name: "Libin WebDev", logo: "assets/bands.jpg"},
+    {name: "lulu", logo: "assets/bands.jpg"},
+    {name: "otf", logo: "assets/bands.jpg"},
+    {name: "cssc", logo: "assets/bands.jpg"},
+    {name: "JPEL Farms", logo: "assets/bands.jpg"},
+    {name: "Libin WebDev", logo: "assets/bands.jpg"},
+       {name: "lulu", logo: "assets/bands.jpg"},
+    {name: "otf", logo: "assets/bands.jpg"},
+    {name: "cssc", logo: "assets/bands.jpg"},
+    {name: "JPEL Farms", logo: "assets/bands.jpg"},
+    {name: "Libin WebDev", logo: "assets/bands.jpg"},
+]
+
+export default partners
+
--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -8,23 +8,25 @@ import running from "../../assets/running.jpg";
 import moreweights from "../../assets/moreweights.jpg";
 import logowithoutwords from "../../assets/logowithoutwords.png";
 import Modal from "../../components/Modal/Modal.jsx";
+import Card from "../../components/Card/Card.jsx";
 // import logosmall from '../../assets/logosmall.png'
+import partners from "../../constants/partners.js";
 
 const HomeScreen = () => {
   const [submissionStatus, setSubmissionStatus] = useState({
     heading: "Welcome",
     message: "Book a FREE Discovery Call with Dr. Wade!",
-    link: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ1EETz6ot_qwrUVyS1SoXryFGwiVbG2x95K22gDuY4CVbJ1KHUqBRfEatmAd_TC_yjMB1cAE2eZ?gv=true'
+    link: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ1EETz6ot_qwrUVyS1SoXryFGwiVbG2x95K22gDuY4CVbJ1KHUqBRfEatmAd_TC_yjMB1cAE2eZ?gv=true",
   });
   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    //2 seconds
-    const timeoutId = setTimeout(() => {
-      setIsOpen(true);
-    }, 2000);
-    return () => clearTimeout(timeoutId);
-  }, []);
+  // useEffect(() => {
+  //   //2 seconds
+  //   const timeoutId = setTimeout(() => {
+  //     setIsOpen(true);
+  //   }, 2000);
+  //   return () => clearTimeout(timeoutId);
+  // }, []);
 
   return (
     <>
@@ -41,10 +43,6 @@ const HomeScreen = () => {
             data-aos-duration="1000"
             data-aos-easing="ease-in-out"
           >
-            {/* <h3>Dr.Christian Wade</h3> */}
-            {/* <h4>
-              Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar
-            </h4> */}
             <h1>
               <span className="blue-color">Physical Therapy </span>and{" "}
               <span className="blue-color">Wellness Coaching </span> <br />
@@ -56,72 +54,49 @@ const HomeScreen = () => {
         </div>
 
         <Testimonials />
+
+        {/* parallax 1 */}
+
         <div
           className="plx-image1"
           title="Weight Lifter"
           alt="Photo by Victor Freitas on Unsplash "
         ></div>
+
+        {/* the process/cards section */}
+
         <div className="process-container">
           <h2>
             The <span className="blue-color">Concierge</span> Process
           </h2>
           <span className="hr-line"></span>
-          <h4>
-            Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar
-          </h4>
-          <p> Rhoncus | Dolor | Purus</p>
+          <h4>Dr. Wade and Concierge Wellness will elevate your well-being</h4>
+          <p> Recovery | Performance | Wellness</p>
           <div className="process-info-container">
-            <div
-              className="process-info"
-              data-aos="zoom-in-right"
-              data-aos-duration="800"
-              data-aos-easing="ease-in-out"
-            >
-              <img src={running} loading="lazy" alt="running"></img>
-              <h4>Heading</h4>
-              <ul>
-                <li>thing one is this long</li>
-                <li>thing two is longer</li>
-                <li>thing three is the longest</li>
-              </ul>
-              <p>
-                Rhoncus dolor purus non enim praesent elementum facilisis leo.
-              </p>
-            </div>
-            <div
-              className="process-info"
-              data-aos="zoom-in-up"
-              data-aos-duration="800"
-              data-aos-easing="ease-in-out"
-            >
-              <img src={fitness} loading="lazy" alt="fitness"></img>
-              <p>
-                Rhoncus dolor purus non enim praesent elementum facilisis leo.
-                Dolor sit amet consectetur adipiscing elit pellentesque.
-                Venenatis cras sed felis eget. Elementum nisi quis eleifend
-                quam.
-              </p>
-            </div>
-            <div
-              className="process-info"
-              data-aos="zoom-in-left"
-              data-aos-duration="800"
-              data-aos-easing="ease-in-out"
-            >
-              <img src={moreweights} loading="lazy" alt="weights"></img>
-              <h4>Heading</h4>
-              <ul>
-                <li>thing one is this long</li>
-                <li>thing two is longer</li>
-                <li>thing three is the longest</li>
-              </ul>
-
-              <p>
-                Rhoncus dolor purus non enim praesent elementum facilisis leo.
-              </p>
-            </div>
+            <Card
+              image={running}
+              alt="runners"
+              heading="Recovery"
+              duration="800"
+            />
+            <Card
+              image={fitness}
+              alt="weight lifter"
+              heading="Performance"
+              duration="1000"
+            />
+            <Card
+              image={moreweights}
+              alt="someone using a weight machine"
+              heading="Wellness"
+              duration="1200"
+            />
           </div>
         </div>
+
+        <CallButton />
+
+        {/* parallax 2 */}
 
         <div
           className="plx-image2"
@@ -129,22 +104,43 @@ const HomeScreen = () => {
           alt="Photo by Victor Freitas on Unsplash "
         ></div>
 
+        {/* google map */}
+
         <div className="location">
           <h2>Our Location</h2>
           <span className="hr-line"></span>
+          <div className="map-container">
           <iframe
-            title="Google Map"
-            src="https://storage.googleapis.com/maps-solutions-xk6pj5ttcx/locator-plus/7rd4/locator-plus.html"
-            width="100%"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3113.4687427470762!2d-104.68597982346914!3d38.70704587176526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87136a96367f2c61%3A0xcb5f3e14715d0214!2s1315%20Lords%20Hill%20Dr%2C%20Fountain%2C%20CO%2080817!5e0!3m2!1sen!2sus!4v1703458236397!5m2!1sen!2sus"
+            width="80%"
             height="500"
-            style={{
-              border: 0,
-              boxShadow: "inset 0px 0px 15px rgba(0, 0, 0, 0.2)",
-            }}
+            allowfullscreen=""
             loading="lazy"
+            style={{padding: "5rem", border: "none"}}
+            referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
+          </div>
+        </div>
+
+        <div className="partners">
+          <h2>Proud Partners</h2>
+          <span className="hr-line"></span>
+          <div className="partner-card-container">
+            {partners.map((partner) => (
+              <div className="partner-card">
+                <img
+                  src={require(`../../${partner.logo}`)}
+                  alt={partner.name}
+                />
+                <p>{partner.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
+
+      {/* pop up  */}
+
       {isOpen && (
         <Modal
           setIsOpen={setIsOpen}
