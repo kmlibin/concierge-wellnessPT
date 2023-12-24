ba02edf0a1975823439b503a8d96df224a38860b
Kelli Libin <km.putnam2@gmail.com>
2023-12-24 11:23:17 -0700

    redid to use float, now responsive. less code now

--- src/screens/Story/MyStoryScreen.jsx
+++ src/screens/Story/MyStoryScreen.jsx
@@ -9,24 +9,17 @@ import content from "../../constants/content";
 const MyStoryScreen = () => {
   return (
     <article className="story" data-aos="fade-up" data-aos-duration="1000">
-      <div className="story-content">
+      <div className="text-wrap-container">
         <div className="story-title">
           <h1>{content.myStory.header}</h1>
           <span className="hr-line"></span>
         </div>
-        <div className="story-image-container">
-          <p>{content.myStory.contentOne}</p>
-          <img src={pullup} className="story-image" alt="story"/>
-        </div>
-        {/* <div className="story-image-container">      */}
-        <p style={{width: "80%", marginTop: "1.5rem"}}>{content.myStory.contentTwo}</p>
-          <div className="multiple-images">
-          <img src={running} className="story-image" alt="story" />
-          <img src={running} className="story-image" alt="story" />
-        </div>
-     
-          {/* <img src={running}  className="story-image" /> */}
-        {/* </div> */}
+        <p>
+          <img className="upper-right-image" src={pullup} alt="weight lifter" />
+          {content.myStory.contentOne}
+          <img className="lower-left-image" src={running} alt="group of runners" />
+          {content.myStory.contentTwo}
+        </p>
       </div>
     </article>
   );
