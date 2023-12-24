81e95b7ea86bf8c87a2c1a27bf06f8e05aeddf36
Kelli Libin <km.putnam2@gmail.com>
2023-12-24 11:50:36 -0700

    added more faqs

--- src/screens/FAQ/faqData.js
+++ src/screens/FAQ/faqData.js
@@ -1,36 +1,46 @@
 const faqData = [
   {
-    question: `What is dry needling?`,
-    answer: ` sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Lacus sed turpis tincidunt id. Vel quam elementum pulvinar etiam non
-        quam lacus. Mattis vulputate enim nulla aliquet porttitor lacus
-        luctus accumsan. Faucibus nisl tincidunt eget nullam non. Aliquam id
-        diam maecenas ultricies mi eget mauris.`,
-  },
-
-  {
-    question: `What is wellness coaching?`,
-    answer: ` sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Lacus sed turpis tincidunt id. Vel quam elementum pulvinar etiam non
-        quam lacus. Mattis vulputate enim nulla aliquet porttitor lacus
-        luctus accumsan. Faucibus nisl tincidunt eget nullam non. Aliquam id
-        diam maecenas ultricies mi eget mauris.`,
-  },
-  {
-    question: `Can you work with clients in another state?`,
-    answer: ` sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Lacus sed turpis tincidunt id. Vel quam elementum pulvinar etiam non
-        quam lacus. Mattis vulputate enim nulla aliquet porttitor lacus
-        luctus accumsan. Faucibus nisl tincidunt eget nullam non. Aliquam id
-        diam maecenas ultricies mi eget mauris.`,
-  },
-  {
     question: `What do you specialize in?`,
     answer: ` sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Lacus sed turpis tincidunt id. Vel quam elementum pulvinar etiam non
         quam lacus. Mattis vulputate enim nulla aliquet porttitor lacus
         luctus accumsan. Faucibus nisl tincidunt eget nullam non. Aliquam id
         diam maecenas ultricies mi eget mauris.`,
+  },
+  {
+    question: `Do I need a referral from my Doctor?`,
+    answer: `This will depend on your state but Colorado has direct access meaning you do NOT need a referral 
+    to see a Doctor of Physical Therapy. If you have questions about your specific state just contact us and 
+    we’d be happy to help.`,
+  },
+  {
+    question: `Can you work with clients in another state?`,
+    answer: `YES! Currently licensed in CO, TX, WA, NH, AZ and offer virtual appointments at your own convenience.`,
+  },
+  {
+    question: `What is dry needling?`,
+    answer: ` Dry needling is a therapeutic technique that involves the insertion of thin, solid needles into specific 
+    points in the body, known as myofascial trigger points to stimulate a healing response.  Unlike acupuncture, 
+    which is rooted in traditional Chinese medicine and focuses on balancing the body's energy flow, dry needling is a Western 
+    medicine technique primarily used for the treatment of musculoskeletal pain and dysfunction.`,
+  },
+  {
+    question: `What is wellness coaching?`,
+    answer: `Wellness coaching is a form of coaching that focuses on enhancing and optimizing an individual's overall well-being. 
+    The goal is to help individuals achieve a state of holistic wellness, encompassing physical, mental, emotional, and sometimes 
+    even spiritual aspects of their lives. As your Wellness coach we’ll work together to set and achieve goals related to health, 
+    lifestyle, and personal development.This will include weekly meetings to establish specific, individualized steps to take
+    to ensure success towards your health and wellness goals. The aim of wellness coaching is to empower 
+    individuals to take control of their health and lead fulfilling lives by making positive and sustainable choices..`,
+  },
+  {
+    question: `Do you take my insurance?`,
+    answer: ` Concierge Wellness operates as an out of network provider with a fee-for-service business model. 
+    This allows us to provide the highest quality of care without the restrictions of insurance companies. 
+    We value our transparency with cost and our full attention during your sessions. There are many barriers that 
+    insurance sets on providers and we chose to step away from the red tape so we can help you how we see fit. 
+    We accept cash, check, Venmo, Cashapp, Zelle, debit/credit cards, and FSA/HSA. 
+    We can also provide you with a superbill to submit to your insurance for possible reimbursement`,
   },
 ];
 
