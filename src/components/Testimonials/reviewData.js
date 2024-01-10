e083a8ff5c1b4ff89ac6c243c987b63f8d371bf1
Kelli Libin <km.putnam2@gmail.com>
2024-01-10 14:04:17 -0700

    added more reviews

--- src/components/Testimonials/reviewData.js
+++ src/components/Testimonials/reviewData.js
@@ -17,19 +17,32 @@ const reviewData = [
         explaining complex concepts. Before meeting him, many aspects of
         physical therapy felt like they were in a foreign language. But he broke
         everything down, ensuring I understood every step of my treatment. This
-        not only helped me physically but gave me peace of mind.
-        <br />
-        <br />I can't express enough how grateful I am for Christian's
-        dedication and expertise. If you're considering physical therapy, look
-        no further than Concierge Wellness. It's not just about the therapy;
-        it's about the personal touch and genuine care.
+        not only helped me physically but gave me peace of mind. I can't express
+        enough how grateful I am for Christian's dedication and expertise. If
+        you're considering physical therapy, look no further than Concierge
+        Wellness. It's not just about the therapy; it's about the personal touch
+        and genuine care.
       </div>
     ),
   },
   {
+    name: "Katie D.",
+    content: `Christian is very professional, kind, and knowledgeable. From setting up the very first appointment, 
+    I could tell that he genuinely cares about his clients lives and overall wellbeing and doesn’t just try to get 
+    them in and out the door. He helped me with an ankle injury, and even though I had to do my meetings over zoom, 
+    I felt more comfortable and taken care of with him than the people that I tried to see in-person. We met virtually, 
+    and Christian was able to give me at-home exercises that were helpful and healing, and he was very intentional with 
+    asking questions and understanding exactly where I’m at even when he couldn’t be there in person. 
+    If I ever have any injuries again, Christian will for sure be the first person I call to set up PT.`,
+  },
+  {
     name: "Keiriel N.",
-    content: `Christian, DPT has been awesome from the beginning of my treatment. Listens to my concerns and what I feel like needs to be better or what is going well and he always has my best interest in mind. Works towards my goals and always has patient-centered care. He is personable and meets me where I am every day I am there. Pushes me to do better within my limits and is always encouraging. 
-    I have never felt judgment coming in if I hadn't met my goal or got behind on my workouts. He is just as encouraging and understanding. I'm so thankful he has been my PT!!!`,
+    content: `Christian, DPT has been awesome from the beginning of my treatment. Listens to my concerns and what I feel like needs 
+    to be better or what is going well and he always has my best interest in mind. Works towards my goals and always has 
+    patient-centered care. He is personable and meets me where I am every day I am there. 
+    Pushes me to do better within my limits and is always encouraging. 
+    I have never felt judgment coming in if I hadn't met my goal or got behind on my workouts. He is just as encouraging and 
+    understanding. I'm so thankful he has been my PT!!!`,
   },
   {
     name: "Kelli L.",
@@ -59,9 +72,14 @@ const reviewData = [
     ),
   },
   {
-    name: "James Doe",
-    content:
-      "Arcu non odio euismod lacinia at quis. Erat imperdiet sed euismod nisi porta lorem mollis aliquam. Urna cursus eget nunc scelerisque viverra mauris. Nibh praesent tristique magna sit amet purus gravida quis blandit. Fames ac turpis egestas sed tempus urna et. Tellus cras adipiscing enim eu turpis. Fringilla ut morbi tincidunt augue interdum.",
+    name: "Priscilla D.",
+    content: `Doctor Wade has helped not only me, but one of my adult children - both virtually. Christian gave me exercises to 
+    help strengthen my ankle after I broke it with a fall. He definitely helped me get back on the ski slopes through 
+    strength training and stretching. The movement in my left foot/ankle now matches the right (which is a HUGE improvement).
+     More importantly, Christian helped our daughter with a foot injury she had overseas. He treated her virtually (over zoom). 
+     It was such a blessing to her to have Christian’s care and expertise and he definitely helped her strengthen and heal. 
+     While some doctors indicated surgery might be needed, it was Christian who insisted she be patient and persistent with her exercises. 
+     It worked! I highly recommend Dr Wade. If you can’t meet with him in person, he will virtually find a way to help.  `,
   },
 ];
 
