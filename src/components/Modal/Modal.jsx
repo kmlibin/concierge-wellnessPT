12bf178b3bee21496520519afc58c7140d902a5c
Kelli Libin <km.putnam2@gmail.com>
2024-01-10 14:30:28 -0700

    added another layer to the modal backdrop

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,11 +7,12 @@ const Modal = ({ setIsOpen, submissionStatus, setSubmissionStatus }) => {
     <>
       <div className="modal-backdrop" onClick={() => setIsOpen(false)} />
       <div className="centered">
-        <div className="modal" data-aos="flip-up" data-aos-duration="500">
-          <div className="modal-header">
-            <h5 className="heading">{submissionStatus.heading}</h5>
-          </div>
-          <button
+        <div className="background-color" data-aos="flip-up" data-aos-duration="500">
+          <div className="modal">
+            <div className="modal-header">
+              <h5 className="heading">{submissionStatus.heading}</h5>
+            </div>
+            <button
             className="top-close-btn"
             onClick={() => {
               setIsOpen(false);
@@ -20,28 +21,35 @@ const Modal = ({ setIsOpen, submissionStatus, setSubmissionStatus }) => {
           >
             <IoClose style={{ marginBottom: "-3px" }} />
           </button>
+            <div className="modal-content">
+              {submissionStatus.link ? (
+                <p>
+                  {submissionStatus.message}
+                  <br />
+                  <br />
+                  <a
+                    style={{ fontWeight: "bold" }}
+                    href={submissionStatus.link}
+                  >
+                    Click Here to Schedule!
+                  </a>
+                </p>
+              ) : (
+                <p>{submissionStatus.message}</p>
+              )}
+            </div>
 
-          <div className="modal-content">
-            {submissionStatus.link ? (
-              <p>
-                {submissionStatus.message}<br/><br/>
-                <a style={{fontWeight: "bold"}} href={submissionStatus.link}>Click Here to Schedule!</a>
-              </p>
-            ) : (
-              <p>{submissionStatus.message}</p>
-            )}
-          </div>
-
-          <div className="actions-container">
-            <button
-              className="close-btn"
-              onClick={() => {
-                setIsOpen(false);
-                setSubmissionStatus(null);
-              }}
-            >
-              Close
-            </button>
+            <div className="actions-container">
+              <button
+                className="close-btn"
+                onClick={() => {
+                  setIsOpen(false);
+                  setSubmissionStatus(null);
+                }}
+              >
+                Close
+              </button>
+            </div>
           </div>
         </div>
       </div>
