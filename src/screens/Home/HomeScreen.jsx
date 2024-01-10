5a63e6bc7e6785cf9c8cb8c71c8805ec777bca0d
Kelli Libin <km.putnam2@gmail.com>
2024-01-10 14:15:52 -0700

    removed partners and map code/scss

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -13,6 +13,8 @@ import Card from "../../components/Card/Card.jsx";
 import partners from "../../constants/partners.js";
 import content from "../../constants/content.js";
 import { Helmet } from "react-helmet-async";
+import Map from "../../components/GoogleMap/Map.jsx";
+import Partners from "../../components/Partners/Partners.jsx";
 
 const HomeScreen = () => {
   const [submissionStatus, setSubmissionStatus] = useState({
@@ -39,6 +41,9 @@ const HomeScreen = () => {
           content="Offering personalized and expert physical therapy services with a focus on your well-being. Located in Colorado Springs, Colorado"
         />
       </Helmet>
+
+      {/* hero section */}
+
       <main className="main-container">
         <div className="logo-container">
           <img
@@ -62,7 +67,6 @@ const HomeScreen = () => {
             </h1>
             <h4>In home services and telehealth available</h4>
           </div>
-        
         </div>
 
         <Testimonials />
@@ -134,19 +138,10 @@ const HomeScreen = () => {
         >
           <h2>Our Location</h2>
           <span className="hr-line"></span>
-          <div className="map-container">
-            <iframe
-              title="Google Map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3113.4687427470762!2d-104.68597982346914!3d38.70704587176526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87136a96367f2c61%3A0xcb5f3e14715d0214!2s1315%20Lords%20Hill%20Dr%2C%20Fountain%2C%20CO%2080817!5e0!3m2!1sen!2sus!4v1703458236397!5m2!1sen!2sus"
-              width="90%"
-              height="500"
-              allowfullscreen=""
-              loading="lazy"
-              style={{ padding: "5rem", border: "none" }}
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
+          <Map />
         </div>
+
+        {/* partners section */}
 
         <div
           className="partners"
@@ -156,17 +151,7 @@ const HomeScreen = () => {
         >
           <h2>Proud Partners</h2>
           <span className="hr-line"></span>
-          <div className="partner-card-container">
-            {partners.map((partner) => (
-              <div className="partner-card">
-                <img
-                  src={require(`../../${partner.logo}`)}
-                  alt={partner.name}
-                />
-                <p>{partner.name}</p>
-              </div>
-            ))}
-          </div>
+          <Partners />
         </div>
       </main>
 
