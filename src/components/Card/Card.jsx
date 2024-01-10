9424af49b29cf3b408185d56e2a9a5b0b9180537
Kelli Libin <km.putnam2@gmail.com>
2024-01-10 12:10:16 -0700

    got bullet points as prop, mapped over

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./Card.scss";
 
-const Card = ({ image, alt, heading, duration }) => {
+const Card = ({ image, alt, heading, duration, bulletpoints }) => {
   return (
     <div className="card-container">
-      {/* <h3 style= {{marginBottom: "1.5rem", fontStyle: "italic", color: "rgb(17, 163, 213)"}}>{heading}</h3> */}
       <div
         className="process-info"
         data-aos="zoom-in-right"
@@ -13,13 +12,13 @@ const Card = ({ image, alt, heading, duration }) => {
       >
         <h3>{heading}</h3>
         <img src={image} loading="lazy" alt={alt}></img>
-
-        <ul>
-          <li>thing one is this long</li>
-          <li>thing two is longer</li>
-          <li>thing three is the longest</li>
-        </ul>
-        <p>Rhoncus dolor purus non enim praesent elementum facilisis leo.</p>
+        <div className="card-text">
+          <ul>
+            {bulletpoints.map((point) => (
+              <li>{point}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
