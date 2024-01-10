4ac515c84a8e8a3a3f30bde787c4ea04896884ad
Kelli Libin <km.putnam2@gmail.com>
2024-01-09 18:46:36 -0700

    add helmet and helmet provider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/Header/NavBar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -10,9 +11,11 @@ const App = () => {
   AOS.init();
   return (
     <>
-      <NavBar />
-      <Outlet />
-      <Footer />
+      <HelmetProvider>
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </HelmetProvider>
     </>
   );
 };
