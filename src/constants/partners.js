e876b4d14a779b1a097991e15416ba15d2621d6f
Kelli Libin <km.putnam2@gmail.com>
2024-01-10 12:10:58 -0700

    added correct logo for libin web dev

--- src/constants/partners.js
+++ src/constants/partners.js
@@ -3,17 +3,7 @@ const partners =  [
     {name: "otf", logo: "assets/bands.jpg"},
     {name: "cssc", logo: "assets/bands.jpg"},
     {name: "JPEL Farms", logo: "assets/bands.jpg"},
-    {name: "Libin WebDev", logo: "assets/bands.jpg"},
-    {name: "lulu", logo: "assets/bands.jpg"},
-    {name: "otf", logo: "assets/bands.jpg"},
-    {name: "cssc", logo: "assets/bands.jpg"},
-    {name: "JPEL Farms", logo: "assets/bands.jpg"},
-    {name: "Libin WebDev", logo: "assets/bands.jpg"},
-       {name: "lulu", logo: "assets/bands.jpg"},
-    {name: "otf", logo: "assets/bands.jpg"},
-    {name: "cssc", logo: "assets/bands.jpg"},
-    {name: "JPEL Farms", logo: "assets/bands.jpg"},
-    {name: "Libin WebDev", logo: "assets/bands.jpg"},
+    {name: "Libin Web Development", logo: "assets/kllogosmaller.png"},
 ]
 
 export default partners
