9e63bbbd21c4e216bf741705d794259ee308c177
Kelli Libin <km.putnam2@gmail.com>
2024-01-10 14:04:05 -0700

    added another layer to the card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,21 +3,23 @@ import "./Card.scss";
 
 const Card = ({ image, alt, heading, duration, bulletpoints }) => {
   return (
-    <div className="card-container">
-      <div
-        className="process-info"
-        data-aos="zoom-in-right"
-        data-aos-duration={duration}
-        data-aos-easing="ease-in-out"
-      >
-        <h3>{heading}</h3>
-        <img src={image} loading="lazy" alt={alt}></img>
-        <div className="card-text">
-          <ul>
-            {bulletpoints.map((point) => (
-              <li>{point}</li>
-            ))}
-          </ul>
+    <div
+      className="card-border"
+      data-aos="zoom-in-right"
+      data-aos-duration={duration}
+      data-aos-easing="ease-in-out"
+    >
+      <div className="card-container">
+        <div className="process-info">
+          <h3>{heading}</h3>
+          <img src={image} loading="lazy" alt={alt}></img>
+          <div className="card-text">
+            <ul>
+              {bulletpoints.map((point) => (
+                <li>{point}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
