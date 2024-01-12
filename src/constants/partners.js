4eccefae028f5220aef37d3d09d711a401f5ac65
Kelli Libin <km.putnam2@gmail.com>
2024-01-12 14:25:34 -0700

    added logos for otf and jpel, remove box shadow

--- src/constants/partners.js
+++ src/constants/partners.js
@@ -1,8 +1,8 @@
 const partners =  [
     {name: "lulu", logo: "assets/bands.jpg"},
-    {name: "otf", logo: "assets/bands.jpg"},
+    {name: "Orange Theory Fitness", logo: "assets/orangetheory.png"},
     {name: "cssc", logo: "assets/bands.jpg"},
-    {name: "JPEL Farms", logo: "assets/bands.jpg"},
+    {name: "JPEL Farms", logo: "assets/field.png"},
     {name: "Libin Web Development", logo: "assets/kllogosmaller.png"},
 ]
 
