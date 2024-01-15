ab521ffdb24e370c76fe143ffb3808ed4e5336e0
Kelli Libin <km.putnam2@gmail.com>
2024-01-14 17:13:01 -0700

    updated order of questions

--- src/screens/FAQ/faqData.js
+++ src/screens/FAQ/faqData.js
@@ -1,13 +1,21 @@
 const faqData = [
   {
-    question: `Do I need a referral from my Doctor?`,
-    answer: `This will depend on your state but Colorado has direct access meaning you do NOT need a referral 
-    to see a Doctor of Physical Therapy. If you have questions about your specific state just contact us and 
-    we’d be happy to help.`,
+    question: `Do you take my insurance?`,
+    answer: ` Concierge Wellness operates as an out of network provider with a fee-for-service business model. 
+    This allows us to provide the highest quality of care without the restrictions of insurance companies. 
+    We value our transparency with cost and our full attention during your sessions. There are many barriers that 
+    insurance sets on providers and we chose to step away from the red tape so we can help you how we see fit. 
+    We accept cash, check, Venmo, Cashapp, Zelle, debit/credit cards, and FSA/HSA. 
+    We can also provide you with a superbill to submit to your insurance for possible reimbursement`,
   },
   {
-    question: `Can you work with clients in another state?`,
-    answer: `YES! Currently licensed in CO, TX, WA, NH, AZ and offer virtual appointments at your own convenience.`,
+    question: `What is wellness coaching?`,
+    answer: `Wellness coaching is a form of coaching that focuses on enhancing and optimizing an individual's overall well-being. 
+    The goal is to help individuals achieve a state of holistic wellness, encompassing physical, mental, emotional, and sometimes 
+    even spiritual aspects of their lives. As your Wellness coach we’ll work together to set and achieve goals related to health, 
+    lifestyle, and personal development. This will include weekly meetings to establish specific, individualized steps to take
+    to ensure success towards your health and wellness goals. The aim of wellness coaching is to empower 
+    individuals to take control of their health and lead fulfilling lives by making positive and sustainable choices..`,
   },
   {
     question: `What is dry needling?`,
@@ -17,22 +25,14 @@ const faqData = [
     medicine technique primarily used for the treatment of musculoskeletal pain and dysfunction.`,
   },
   {
-    question: `What is wellness coaching?`,
-    answer: `Wellness coaching is a form of coaching that focuses on enhancing and optimizing an individual's overall well-being. 
-    The goal is to help individuals achieve a state of holistic wellness, encompassing physical, mental, emotional, and sometimes 
-    even spiritual aspects of their lives. As your Wellness coach we’ll work together to set and achieve goals related to health, 
-    lifestyle, and personal development.This will include weekly meetings to establish specific, individualized steps to take
-    to ensure success towards your health and wellness goals. The aim of wellness coaching is to empower 
-    individuals to take control of their health and lead fulfilling lives by making positive and sustainable choices..`,
+    question: `Can you work with clients in another state?`,
+    answer: `YES! Currently licensed in CO, TX, WA, NH, AZ and offer virtual appointments at your own convenience.`,
   },
   {
-    question: `Do you take my insurance?`,
-    answer: ` Concierge Wellness operates as an out of network provider with a fee-for-service business model. 
-    This allows us to provide the highest quality of care without the restrictions of insurance companies. 
-    We value our transparency with cost and our full attention during your sessions. There are many barriers that 
-    insurance sets on providers and we chose to step away from the red tape so we can help you how we see fit. 
-    We accept cash, check, Venmo, Cashapp, Zelle, debit/credit cards, and FSA/HSA. 
-    We can also provide you with a superbill to submit to your insurance for possible reimbursement`,
+    question: `Do I need a referral from my Doctor?`,
+    answer: `This will depend on your state but Colorado has direct access meaning you do NOT need a referral 
+    to see a Doctor of Physical Therapy. If you have questions about your specific state just contact us and 
+    we’d be happy to help.`,
   },
 ];
 
