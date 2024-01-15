b11e29f6f13fbae9ca672c406cd26dd2b3670b58
Kelli Libin <km.putnam2@gmail.com>
2024-01-15 16:05:29 -0700

    added accordion for small screens

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaCaretDown, FaCaretUp } from "react-icons/fa";
 import "./Card.scss";
 
 const Card = ({ image, alt, heading, duration, bulletpoints, tagline }) => {
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsAccordionOpen(!isAccordionOpen);
+  };
+
   return (
     <div
-      className="card-border"
-      data-aos="zoom-in-right"
-      data-aos-duration={duration}
-      data-aos-easing="ease-in-out"
+      className={`card-border ${isAccordionOpen ? "accordion-mode" : ""}`}
+      // data-aos="zoom-in-right"
+      // data-aos-duration={duration}
+      // data-aos-easing="ease-in-out"
     >
       <div className="card-container">
         <div className="process-info">
           <h3>{heading}</h3>
           <hr className="grey-hr-line"></hr>
           <img src={image} loading="lazy" alt={alt}></img>
-          <div className="card-text">
+          <div
+            className={`accordion-indicator ${isAccordionOpen ? "open" : ""}`}
+            onClick={toggleAccordion}
+          >
+            <FaCaretDown />
+          </div>
+          <div className={`card-text ${isAccordionOpen ? "open" : ""}`}>
             <ul>
               {bulletpoints.map((point) => (
                 <li>{point}</li>
