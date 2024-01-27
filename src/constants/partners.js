09e5604be9749bc000ac82b176d1e166e1df965e
Kelli Libin <km.putnam2@gmail.com>
2024-01-27 15:22:52 -0700

    added links, added logo to cssc, removed lulu

--- src/constants/partners.js
+++ src/constants/partners.js
@@ -1,9 +1,8 @@
 const partners =  [
-    {name: "lulu", logo: "assets/bands.jpg"},
-    {name: "Orange Theory Fitness", logo: "assets/orangetheory.png"},
-    {name: "cssc", logo: "assets/bands.jpg"},
+    {name: "Orange Theory Fitness", logo: "assets/orangetheory.png", link: "https://www.orangetheory.com"},
+    {name: "CSSC", logo: "assets/cssclogo.jpg", link: "https://www.coloradospringssports.org/"},
     {name: "JPEL Farms", logo: "assets/field.png"},
-    {name: "Libin Web Development", logo: "assets/kllogosmaller.png"},
+    {name: "Libin Web Development", logo: "assets/kllogosmaller.png", link: "https://kelli-portfolio.netlify.app"},
 ]
 
 export default partners
