f3c5facc448f8674f84f265ddc0c6dc19a1e4333
Kelli Libin <km.putnam2@gmail.com>
2024-01-27 15:23:21 -0700

    wrapped image an text in anchor

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -10,9 +10,11 @@ const Partners = () => {
   return (
     <div className="partner-card-container">
       {partners.map((partner) => (
-        <div className="partner-card">
-          <img src={require(`../../${partner.logo}`)} alt={partner.name} />
-          <p>{partner.name}</p>
+        <div className="partner-card" key={partner.name}>
+          <a href={partner.link} target="_blank" rel="noopener noreferrer">
+            <img src={require(`../../${partner.logo}`)} alt={partner.name} />
+            {partner.name}
+          </a>
         </div>
       ))}
     </div>
