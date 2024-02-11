786de0ec2e553d8ca9808ca16237132384f42da7
Kelli Libin <km.putnam2@gmail.com>
2024-02-11 15:50:04 -0700

    add text content

--- src/screens/WellnessCoaching/wellnesscontent.js
+++ src/screens/WellnessCoaching/wellnesscontent.js
@@ -1,17 +1,17 @@
 const wellnesscontent = {
-    header: "Wellness Coaching",
-    subheading: "Become a Healthier You!",
-    summary:
-      "Empowering your journey to wellness, our dedicated physical therapy services offer personalized assessments and expert treatments. Discover a tailored approach for your optimal health",
-    listOne: [
-      "Comprehensive initial consultation",
-      "Individualized goal-setting",
-      "Personal habit tracking",
-      "Weekly review of progress",
-      "Video, call, or email check-ins",
-      "Access to workout programs",
-      "Corporate wellness programs"
-    ],
-  }
+  header: "Wellness Coaching",
+  subheading: "Become a Healthier You!",
+  summary:
+    "Embark on a journey to better health with our Wellness Coaching services. From personalized goal-setting to regular progress reviews, we provide comprehensive support to help you achieve your wellness goals effectively.",
+  listOne: [
+    "Comprehensive initial consultation",
+    "Individualized goal-setting",
+    "Personal habit tracking",
+    "Weekly review of progress",
+    "Video, call, or email check-ins",
+    "Access to workout programs",
+    "Corporate wellness programs",
+  ],
+};
 
-  export default wellnesscontent
+export default wellnesscontent;
