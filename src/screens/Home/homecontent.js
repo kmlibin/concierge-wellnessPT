3d8ea0a6e1feccd25a427da7e9ef239aec91362c
Kelli Libin <km.putnam2@gmail.com>
2024-02-11 12:03:29 -0700

    added/took away a couple words

--- src/screens/Home/homecontent.js
+++ src/screens/Home/homecontent.js
@@ -1,15 +1,15 @@
 const homecontent = {
   home: {
     bulletpointsOne: [
-      "Calm down current symptoms",
-      "Restore pain free mobility",
+      "Calm down your current symptoms",
+      "Restore your pain free mobility",
       "Initiate corrective exercises",
       // "Learn active recovery strategies",
     ],
     bulletpointsTwo: [
       "Adapt a comprehensive exercise program",
       "Increase overall fitness greater than baseline",
-      "Learn how to manage symptoms if they return",
+      "Learn to manage symptoms if they return",
     ],
     bulletpointsThree: [
       "Create sustainable lifestyle habits",
