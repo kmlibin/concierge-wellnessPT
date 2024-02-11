77f4170e891a39aaf98694a130db98cd82ebd535
Kelli Libin <km.putnam2@gmail.com>
2024-02-11 15:44:39 -0700

    split into their own respsective files

---
+++ src/screens/PT/ptcontent.js
@@ -0,0 +1,19 @@
+const ptcontent = {
+  header: "Physical Therapy",
+  subheading: "Achieve Recovery and Mobility",
+  summary:
+    "Empowering your journey to wellness, our dedicated physical therapy services offer personalized assessments and expert treatments. Discover a tailored approach for your optimal health.",
+  listOne: [
+    "Myofascial decompression",
+    "Joint mobilization",
+    "Therapeautic massage",
+    "Corrective exercise prescription",
+    "Trigger point dry needling",
+    "Spinal manipulation",
+    "Exercise programming",
+    "Ergonomic assessments",
+    "Group fitness classes",
+  ],
+};
+
+export default ptcontent;
---
+++ src/screens/WellnessCoaching/wellnesscontent.js
@@ -0,0 +1,17 @@
+const wellnesscontent = {
+    header: "Wellness Coaching",
+    subheading: "Become a Healthier You!",
+    summary:
+      "Empowering your journey to wellness, our dedicated physical therapy services offer personalized assessments and expert treatments. Discover a tailored approach for your optimal health",
+    listOne: [
+      "Comprehensive initial consultation",
+      "Individualized goal-setting",
+      "Personal habit tracking",
+      "Weekly review of progress",
+      "Video, call, or email check-ins",
+      "Access to workout programs",
+      "Corporate wellness programs"
+    ],
+  }
+
+  export default wellnesscontent
