40ed431e86b7c2882398020dd9d8888af2bfa17d
Kelli Libin <km.putnam2@gmail.com>
2024-02-15 15:14:02 -0700

    adjusted blurb

--- src/screens/Contact/contactcontent.js
+++ src/screens/Contact/contactcontent.js
@@ -1,6 +1,6 @@
 const contactcontent = {
   blurb:
-    "Have questions about our services, want to learn more about our approach to physical therapy, or have inquiries about the doc? We're here to help! Fill out the form below with your details and any questions you may have. Whether it's about scheduling, specific treatments, or anything else related to physical therapy, we're eager to assist you. Your well-being is our priority, and we look forward to connecting with you!",
+    "Have questions about our services, want to learn more about our approach to physical therapy, or have inquiries about the doc? We're here to help! Fill out the form with your details and any questions you may have. Whether it's about scheduling, specific treatments, or anything else related to your health and wellness, your well-being is our priority and we look forward to connecting with you!",
 };
 
 export default contactcontent;
