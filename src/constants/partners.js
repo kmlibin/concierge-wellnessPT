4fa62c9f785ed0f9d76fe5b40797ef53e5c3b8ad
Kelli Libin <km.putnam2@gmail.com>
2024-02-16 10:05:33 -0700

    new logos for jpel and libinweb

--- src/constants/partners.js
+++ src/constants/partners.js
@@ -1,8 +1,8 @@
 const partners =  [
     {name: "Orange Theory Fitness", logo: "assets/orangetheory.png", link: "https://www.orangetheory.com"},
     {name: "CSSC", logo: "assets/cssclogo.jpg", link: "https://www.coloradospringssports.org/"},
-    {name: "JPEL Farms", logo: "assets/field.png"},
-    {name: "Libin Web Development", logo: "assets/kllogosmaller.png", link: "https://kelli-portfolio.netlify.app"},
+    {name: "JPEL Farms", logo: "assets/newlogo2.png"},
+    {name: "Libin Web Development", logo: "assets/kmllogo.png", link: "https://kelli-portfolio.netlify.app"},
 ]
 
 export default partners
