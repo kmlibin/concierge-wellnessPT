ac58e93820022c3802c7f43063fe7fe6bb1931bd
Kelli Libin <km.putnam2@gmail.com>
2024-02-29 11:12:30 -0700

    added new partner

--- src/constants/partners.js
+++ src/constants/partners.js
@@ -1,9 +1,25 @@
-const partners =  [
-    {name: "Orange Theory Fitness", logo: "assets/orangetheory.png", link: "https://www.orangetheory.com"},
-    {name: "CSSC", logo: "assets/cssclogo.jpg", link: "https://www.coloradospringssports.org/"},
-    {name: "JPEL Farms", logo: "assets/newlogo2.png"},
-    {name: "Libin Web Development", logo: "assets/kmllogo.png", link: "https://kelli-portfolio.netlify.app"},
-]
+const partners = [
+  {
+    name: "Orange Theory Fitness",
+    logo: "assets/orangetheory.png",
+    link: "https://www.orangetheory.com",
+  },
+  {
+    name: "CSSC",
+    logo: "assets/cssclogo.jpg",
+    link: "https://www.coloradospringssports.org/",
+  },
+  { name: "JPEL Farms", logo: "assets/newlogo2.png" },
+  {
+    name: "Libin Web Development",
+    logo: "assets/kmllogo.png",
+    link: "https://kelli-portfolio.netlify.app",
+  },
+  {
+    name: "Pulse Wellness",
+    logo: "assets/pulse.webp",
+    link: "https://www.pulsewellnessco.com/",
+  },
+];
 
-export default partners
-
+export default partners;
