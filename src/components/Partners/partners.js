9ec47ca1b82955e2eeae08e1dbb2075b309d4737
Kelli Libin <km.putnam2@gmail.com>
2024-03-07 11:38:14 -0700

    delete some partners

---
+++ src/components/Partners/partners.js
@@ -0,0 +1,15 @@
+const partners = [
+  { name: "JPEL Farms", logo: "assets/newlogo2.png" },
+  {
+    name: "Libin Web Development",
+    logo: "assets/kmllogo.png",
+    link: "https://kelli-portfolio.netlify.app",
+  },
+  {
+    name: "Pulse Wellness",
+    logo: "assets/pulse.webp",
+    link: "https://www.pulsewellnessco.com/",
+  },
+];
+
+export default partners;
