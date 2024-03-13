4cd095cc7d7f28b057b7b81f14df9bff4adf8229
Kelli Libin <km.putnam2@gmail.com>
2024-03-13 11:40:56 -0600

    updated name of file, added text to the p

---
+++ src/components/Partners/ProudPartners.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+//styles
+import "./Partners.scss";
+
+//content
+import partners from "./partners.js";
+
+const Partners = () => {
+  return (
+
+    <div
+          className="partners"
+          data-aos="fade-up"
+          data-aos-duration="800"
+          data-aos-easing="ease-in-out"
+        >
+          <h2>Proud Partners</h2>
+          <span className="hr-line"></span>{" "}
+          <p>
+            We are always looking to work with like-minded small businesses to improve our
+            community’s health and wellness. Email Concierge Wellness for
+            inquiries.
+          </p>
+     
+     
+    <div className="partner-card-container">
+      {partners.map((partner) => (
+        <div className="partner-card" key={partner.name}>
+          <a href={partner.link} target="_blank" rel="noopener noreferrer">
+            <img src={require(`../../${partner.logo}`)} alt={partner.name} />
+            {partner.name}
+          </a>
+        </div>
+      ))}
+    </div>   
+    </div>
+  );
+};
+
+export default Partners;
